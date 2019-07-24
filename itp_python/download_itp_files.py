3ea595e6f1573c96e881c243ffa4cfffb730f464
Jeff Grant <jeffery.grant@gmail.com>
2019-07-23 21:19:29 -0400

    adds md5 hash check to the downloader.

--- itp_python/download_itp_files.py
+++ itp_python/download_itp_files.py
@@ -1,6 +1,7 @@
 import re
-import os
 import ftputil
+from hashlib import md5
+from pathlib import Path
 
 
 """
@@ -11,24 +12,48 @@ on the FTP site.
 """
 
 
-print('Connecting')
-local_directory = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), 'files'))
-r = re.compile(r'itp[0-9]*final.zip')
+def parse_checksums(path):
+    md5_dict = {}
+    with open(path, 'r') as fid:
+        for line in fid:
+            md5_hash, file = line.split()
+            md5_dict[file] = md5_hash
+    return md5_dict
 
-with ftputil.FTPHost('ftp.whoi.edu', 'anonymous', 'guest') as ftp_host:
-    print('Changing directory')
-    ftp_host.chdir('/whoinet/itpdata/')
-    files = ftp_host.listdir(ftp_host.curdir)
-    final_files = list(filter(r.match, files))
-    print('Getting file list')
-    print('Downloading Files')
-    new_files = False
-    for file_name in final_files:
-        print('Downloading ' + file_name)
-        state = ftp_host.download_if_newer(
-            file_name,
-            os.path.join(local_directory, file_name))
-        new_files = new_files or state
-if new_files:
-    print('New files downloaded')
+
+def calc_md5(path):
+    return md5(open(path, 'rb').read()).hexdigest()
+
+
+def download_itp_files(destination):
+    r = re.compile(r'itp[0-9]*final.zip')
+    with ftputil.FTPHost('ftp.whoi.edu', 'anonymous', 'guest') as ftp_host:
+        print('Changing directory')
+        ftp_host.chdir('/whoinet/itpdata/')
+        print('Getting file list')
+        files = ftp_host.listdir(ftp_host.curdir)
+        final_files = list(filter(r.match, files))
+        print('{} files found'.format(len(final_files)))
+        ftp_host.download('MD5SUMS', destination / 'md5.txt')
+        checksums = parse_checksums(destination / 'md5.txt')
+        new_files = False
+        for file_name in final_files:
+            local_path = destination / file_name
+            state = ftp_host.download_if_newer(file_name, local_path)
+            if calc_md5(local_path) != checksums[file_name]:
+                local_path.unlink()
+                print('{} failed the checksum test. The file has been '
+                      'deleted. Please try running this script '
+                      'again.'.format(file_name))
+                raise ValueError('{} failed checksum test'.format(file_name))
+            if state:
+                print(file_name + ' downloaded')
+                new_files = True
+
+    if new_files:
+        print('New files downloaded')
+    else:
+        print('All files up to date')
+
+if __name__ == '__main__':
+    download_itp_files(Path() / 'files')
