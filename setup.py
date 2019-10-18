4a5a05663f413a52ec85bf972c678898d85b6892
Jeff Grant <jeffery.grant@gmail.com>
2019-10-18 15:33:44 -0400

    update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
+
 setup(
     name='itp_python',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/WHOI-ITP/ITP-Python',
     author='Jeff Grant',
     author_email='jeffery.grant@gmail.com',
-    description='Python software for accessing and manipulating Ice Tethered '
-                'Profiler data',
-    packages=['itp_python']
+    description='Python software for accessing and manipulating Ice '
+                'Tethered Profiler data',
+    packages=['itp_python', ],
+    setup_requires=['h5py', 'numpy', 'pytest', 'pytest-cov', 'pytest-mock']
 )
