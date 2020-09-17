3bbb0cde7a5db6b9d04de04b21805a856b4d6222
Jeff <jeffery.grant@gmail.com>
2020-09-17 08:20:51 -0400

    adds support for old style .mat files

--- admin_tools/cormat.py
+++ admin_tools/cormat.py
@@ -5,6 +5,7 @@ from collections.abc import Iterable
 from pathlib import Path
 from datetime import datetime
 from admin_tools.itp import ItpProfile
+from scipy.io import loadmat
 
 
 def to_string(array):
@@ -38,18 +39,29 @@ class CormatParser:
         self.metadata = {}
         self.variables = {}
         self.filename_re = re.compile(r'cor([0-9]+).mat')
+        self.is_hdf5 = False
 
     def parse(self):
         try:
-            with h5py.File(self.path, 'r') as file:
-                if not self.check_required_dsets(file):
-                    return
-                self.parse_metadata(file)
-                self.parse_data(file)
-                if len(self.variables['pressure']) != 0:
-                    return ItpProfile(self.metadata, self.variables)
-        except OSError:
-            pass
+            if h5py.is_hdf5(self.path):
+                self.is_hdf5 = True
+                with h5py.File(self.path, 'r') as file:
+                    return self._parse(file)
+            else:
+                file = loadmat(self.path)
+                return self._parse(file)
+        except OSError as e:
+            print(f'Error reading {self.path}')
+            print(e)
+
+    def _parse(self, file):
+        if not self.check_required_dsets(file):
+            print('Required data missing')
+            return
+        self.parse_metadata(file)
+        self.parse_data(file)
+        if len(self.variables['pressure']) != 0:
+            return ItpProfile(self.metadata, self.variables)
 
     def check_required_dsets(self, file):
         if self.REQUIRED.issubset(file.keys()):
@@ -64,18 +76,25 @@ class CormatParser:
         m['system_number'] = int(np.array(file.get('itpno'))[0][0])
         m['profile_number'] = int(re.search(self.filename_re,
                                             self.path.name).group(1))
-        time_str = '{} {}'.format(
-            to_string(np.array(file.get('psdate'))),
-            to_string(np.array(file.get('pstart'))))
+        if self.is_hdf5:
+            time_str = '{} {}'.format(
+                to_string(np.array(file.get('psdate'))),
+                to_string(np.array(file.get('pstart'))))
+        else:
+            time_str = f"{file.get('psdate')[0]} {file.get('pstart')[0]}"
         date_time = datetime.strptime(time_str, '%m/%d/%y %H:%M:%S')
         m['date_time'] = datetime.strftime(date_time, '%Y-%m-%dT%H:%M:%S')
 
     def parse_data(self, file):
         v = self.variables
+        if self.is_hdf5:
+            ind = np.s_[0]
+        else:
+            ind = np.s_[:, 0]
         data = np.stack([
-            np.array(file.get('pr_filt'))[0],
-            np.array(file.get('te_cor'))[0],
-            np.array(file.get('sa_adj'))[0]])
+            np.array(file.get('pr_filt'))[ind],
+            np.array(file.get('te_cor'))[ind],
+            np.array(file.get('sa_adj'))[ind]])
         is_nan_col = np.isnan(data).any(axis=0)
         data = data[:, ~is_nan_col]
         if self.metadata['profile_number'] % 2 == 1:
