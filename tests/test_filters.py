7de482fc8bd20d498eb85cd1d34f5b45fd990922
jeff101 <KOsedeevFinjo8O>
2022-03-07 10:35:58 -0500

    full coverage on test_filters.py

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -5,10 +5,24 @@ from itp.filters import (
     LatitudeFilter,
     LongitudeFilter,
     DateTimeFilter,
+    PressureFilter,
     ExtraVariableFilter,
     pre_filter_factory
 )
 from datetime import datetime
+
+
+class DummyAbstractClass(SqlFilter):
+    def __init__(self):
+        pass
+
+
+def test_abstract_class():
+    dummy = DummyAbstractClass()
+    with pytest.raises(NotImplementedError):
+        dummy._check()
+    with pytest.raises(NotImplementedError):
+        dummy.value()
 
 
 @pytest.mark.parametrize('name, klass', [
@@ -37,7 +51,8 @@ def test_factory_unknown_type():
 @pytest.mark.parametrize('klass', [
     LatitudeFilter,
     LongitudeFilter,
-    DateTimeFilter
+    DateTimeFilter,
+    PressureFilter
 ])
 def test_wrong_n_inputs(klass):
     with pytest.raises(ValueError):
@@ -128,3 +143,26 @@ def test_date_time_filter():
     assert sql == '(date_time BETWEEN ? AND ?)'
     iso_times = [x.strftime('%Y-%m-%dT%H:%M:%S') for x in args]
     assert return_args == iso_times
+
+
+def test_pressure_filter_top_lt_bottom():
+    with pytest.raises(ValueError):
+        PressureFilter([2, 1])
+
+
+def test_pressure_filter():
+    args = [1, 2]
+    sql, return_args = PressureFilter(args).value()
+    assert sql == '(pressure >= ? AND pressure <= ?)'
+    assert return_args == pytest.approx([10000, 20000])
+
+
+def test_extra_variable_filter():
+    args = ['dissolved_oxygen', 'par']
+    sql, return_args = ExtraVariableFilter(args).value()
+    expected = 'profiles.id IN (SELECT profile_id FROM profile_extra_variables ' \
+        'INNER JOIN variable_names ON profile_extra_variables.variable_id ' \
+        '== variable_names.id WHERE variable_names.name IN ' \
+        '(?,?))'
+    assert sql == expected
+    assert return_args == args
