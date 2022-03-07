e130e8b2db7c06070c742b41f6f9c08caadec865
jeff101 <KOsedeevFinjo8O>
2022-03-07 12:10:04 -0500

    remove an admin file from repo

--- setup_venv.bat
+++
@@ -1,6 +0,0 @@
-if exist venv rmdir /S /Q venv
-python -m venv venv
-call venv\scripts\activate.bat
-pip install -e . --no-cache-dir
-pip install -r requirements_dev.txt
-call venv\scripts\deactivate.bat
